70d8055adf19971a4b9f5b07de2da703459ec9be
Naz Demirsoy <75198796+pandacornnny@users.noreply.github.com>
2022-04-13 10:35:02 +0200

    Update xor.py

--- xor/xor.py
+++ xor/xor.py
@@ -24,3 +24,22 @@ print(operator.xor(False,False))
 # True
 # True
 # False
+
+############################################################################################################
+
+from random import seed
+from random import random
+
+def xor(text, key):
+    return "".join([chr(ord(c1) ^ ord(c2)) for (c1,c2) in zip(text,key)])
+
+cipherHex = '4c5c4117525f475e52195a4d'
+
+#possibly from 10 up to 18
+i = 10
+for i in range(18):
+    key = '2022-02-10 ' + str(i)
+    seed(key)
+    cipher = bytearray.fromhex(cipherHex).decode("utf-8")
+    plain = xor(cipher, str(random())[2:])
+    print("Case " + str(i) + ": " + plain)
