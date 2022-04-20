8bdc0f41c8c68b84fe9fa9618dd98d0628c32d6e
Naz Demirsoy <75198796+pandacornnny@users.noreply.github.com>
2022-04-20 20:27:38 +0200

    Delete Hash Function directory

--- Hash Function/hashFunction.py
+++
@@ -1,4 +0,0 @@
-# one-way : infeasible to invert
-# small change to a message should change the hash value so extensively that the new hash value appears uncorrelated with the old hash value
-# It is infeasible to find two different messages with the same hash value
-# quick to compute the hash value for any given message 
